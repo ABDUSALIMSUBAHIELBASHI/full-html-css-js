59938752fd2fffc84d086fe5b755384aff41e2e4
ABDUSALIMSUBAHIELBASHI <salimabdu759@gmail.com>
2025-10-08 11:52:29 +0100

    hi

---
+++ Desktop/1 js/index.js
@@ -0,0 +1,21 @@
+let name = " Abdu salim subahi  ";
+console.log(name)
+let age =20;
+console.log(age==20)
+let country ="sudan";
+console.log(country)
+let ismail = true;
+console.log(ismail)
+let fruits = ["apple","banana","orange"];
+console.log(fruits)
+let person = {name : "Abdu salim subahi", age :20, country :"sudan"};
+console.log (person)
+function greet (name){console.log("hello "== name)}
+greet ("Abdu")
+for ( let i =0; i <  5; i++ ){console.log(i)}
+let email = "email";
+console.log(email.includes("mail"))
+let city = "khartoum";
+console.log (city.toLocaleLowerCase)
+let number = 10.5;
+console.log(Math.round(number ))
